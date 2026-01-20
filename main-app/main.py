aa1f9eadf6af0f66ed7a2c7de795545303736c4d
Robert-Handler <roberthandler49@gmail.com>
2026-01-20 15:21:47 -0500

    Initial release main.py starter 1-20-2026

---
+++ main-app/main.py
@@ -0,0 +1,35 @@
+
+"""
+Crafting System - Main Program (CLI Shell)
+-----------------------------------------
+Purpose:
+  - Provide a user interface for the crafting system.
+  - Communicate with multiple microservices (Auth, Inventory, Projects, etc.).
+  - Maintain an auth token across requests after login.
+
+How to run:
+  python main.py
+
+Config:
+  You can override service URLs with environment variables:
+    AUTH_URL=http://localhost:5000
+    INVENTORY_URL=http://localhost:5001
+    PROJECT_URL=http://localhost:5002
+
+Requirements:
+  pip install requests
+"""
+
+import os
+import sys
+import json
+import getpass
+from typing import Optional, Dict, Any
+
+# ----------------------------
+# Configuration & Environment
+# ----------------------------
+
+AUTH_URL = os.getenv("AUTH_URL", "http://localhost:5000")
+INVENTORY_URL = os.getenv("INVENTORY_URL", "http://localhost:5001")
+PROJECT_URL = os.getenv("PROJECT_URL", "http://localhost:5002")
